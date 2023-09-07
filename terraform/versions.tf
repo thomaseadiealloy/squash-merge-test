47b4e3dbece9e12a934378687ef1d33a01f4b30b
Thomas Eadie <thomas.eadie@alloy.ai>
2023-09-07 12:50:32 -0700

    adding packages

---
+++ terraform/versions.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.72.1"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "4.72.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.10.1"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.1"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.13.0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "2.21.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
